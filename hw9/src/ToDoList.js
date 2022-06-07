a97017719c6fa8a4eb97a243967356bace0939ce
zylin1110 <100752242+zylin1110@users.noreply.github.com>
2022-06-07 15:37:11 +0800

    Add files via upload

---
+++ hw9/src/ToDoList.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+class ToDoList extends React.Component { 
+  constructor(props) { 
+    super(props); 
+    this.state = { 
+      value: '', 
+      itemList: [], 
+    }; 
+    this.handleChange = this.handleChange.bind(this); 
+    this.handleSubmit = this.handleSubmit.bind(this); 
+  } 
+  handleChange(event) { 
+    this.setState({value: event.target.value}); 
+  } 
+  handleSubmit(event) { 
+    this.state.itemList.push(this.state.value); 
+    this.setState({value: '', itemList: this.state.itemList,}); 
+    event.preventDefault(); 
+  } 
+  render()
+  {
+    return ( 
+      <div> 
+      <form onSubmit={this.handleSubmit}> 
+        <label> 
+        &emsp;&emsp;CGU ToDoList&#xFF1A;
+        &ensp;
+          <input type="text" value={this.state.value} onChange={this.handleChange} /> 
+        </label> 
+        &ensp;
+        <input type="submit" value="&#x65B0;&#x589E;" />
+      </form> 
+      <ul style={{ textAlign: 'left'}}>
+        {this.state.itemList.map((item, index) => <li key={`item_${index}`}>
+        {<div id={index}>
+        {<div class="Left_Button"><button>&#x505A;&#x5B8C;</button></div>}{<div class="Item">{item}</div>}{<div class="Right_Button"><button onClick={() => {var obj = document.getElementById(index);obj.innerHTML = "";}}>&#x522A;&#x9664;</button></div>}
+        </div>}
+        </li>)}
+      </ul> 
+      </div> 
+    ); 
+  }
+}
+
+export default ToDoList;
